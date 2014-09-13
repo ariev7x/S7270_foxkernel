20affa112b952fe5927c919d4b227bc5bf23de53
TimofeyFox <timboy98@yandex.ru>
2014-09-13 12:18:56 +0400

    Upgrade Kernel Source Code

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Fri Sep 12 17:47:07 MSK 2014"
+#define LINUX_COMPILE_BY "fox"
+#define LINUX_COMPILE_HOST "FoxPC"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
